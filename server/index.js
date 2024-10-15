04a45e2c06e3111cedeec3499879b5f5c037d809
reginaldbdanso <reginaldbdanso@gmail.com>
2024-10-15 00:46:02 +0000

    Refactor server/index.js for better code organization and error handling

--- server/index.js
+++ server/index.js
@@ -15,10 +15,10 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
+  console.log(req.path, req.method);
+  next();
 })
- 
+
 app.use('/api/posts', postRoutes);
 app.use('/api/blogs', blogRoutes);
 app.use('/api/user', userRoutes);
@@ -27,10 +27,12 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('MongoDB connection error:', error));
+  .then(() => console.log('Connected to MongoDB'))
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error('MongoDB connection error:', error));
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
